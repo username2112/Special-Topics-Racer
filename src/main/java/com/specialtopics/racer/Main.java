35009b416c5c5d5bd4b9df99f85ee838ff1b7797
s-oronae <s-oronae@52L112-13.it.bsd405.org>
2020-02-04 14:40:03 -0800

    Setting up project

---
+++ src/main/java/com/specialtopics/racer/Main.java
@@ -0,0 +1,9 @@
+package com.specialtopics.racer;
+
+public class Main {
+
+	public static void main(String[] args) {
+		new Racer().run();
+	}
+
+}
---
+++ src/main/java/com/specialtopics/racer/Racer.java
@@ -0,0 +1,7 @@
+package com.specialtopics.racer;
+
+public class Racer {
+	public void run() {
+		System.out.println("Running!");
+	}
+}
